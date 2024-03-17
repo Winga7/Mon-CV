4ae1be6b1689901c6cf174b6e13fbd5b309f7418
Winga7 <andywinga@hotmail.be>
2024-03-17 19:43:06 +0100

    Mise à jour du style CSS, ajout d'une liste sans puces dans la section "autres", ajout d'un script dans la section "app" de la page index.html, mise à jour des informations de contact dans le fichier cv.json + Fin exercices

--- main.js
+++ main.js
@@ -1,141 +1,97 @@
 import "./style.css";
-// import cv from "/cv.json";
+import cv from "/cv.json";
+
+const skills = cv.competencespro.map((skill) => {return `<li><img src="icon/check.png" alt="check">${skill}</li>`}).join("")
+const langues = cv.langues.map((lang) => {return `<li>${lang}</li>`}).join("")
+const centre = cv.centreDInteret.map((int) => {return `<li>${int}</li>`}).join("")
+const competences = (comps) => {
+    if(comps != null){
+        return comps.map((comp) => {return `<li>${comp}</li>`}).join("")
+    } else {
+        return ""
+    }
+}
+const experiences = cv.experiences.map((exp) => {
+    return `
+    <div class="section">
+        <p><strong>${exp.anneedeb}<img src="icon/prochain.png" alt="fleche-droite">${exp.anneefin}</strong>
+        </p>
+        <p>${exp.poste}<em class="text-vert"> ${exp.societe}</em></p>
+
+        <ul>
+            ${competences(exp.competences ? exp.competences : null)}
+        </ul>
+    </div>`
+}).join("")
+const formation = cv.etudeformation.map((form) => {return `<p>
+    <strong>
+    ${form.anneedeb} <img src="icon/prochain.png" alt="fleche-droite"> ${form.anneefin}
+    </strong>
+    <em class="text-vert">
+        ${form.lieu}
+    </em>
+</p>
+<ul>
+    ${competences(form.competences)}
+</ul>`}).join("")
+const autres = cv.autreexperiences.map((autre) => {return `<li>${autre}</li>`}).join("")
 
 document.querySelector("#app").innerHTML = `
-
-`;
-
-app.innerHTML = `
 <div class="CV-Conteneur">
 <div class="Colonne-Gauche">
     <img class="portrait" src="images/bandicam_2021-03-13_23-05-00-707.png" alt="Portrait">
     <div class="section">
     <p>
-        <img src="icon/linkedin.png" alt="Icone linkedin"> Andy Petitjean
+        <img src="icon/linkedin.png" alt="Icone linkedin"> ${cv.prenom} ${cv.nom}
     </p>
     </div>
     <div class="section">
     <h2>À PROPOS</h2>
     <p>
-        De nature débrouillard et indépendant dans mon travail, j’aime apprendre de nouvelles technologies.
-        <br>
-        Mes valeurs de travail : flexibilité, sang-froid et sérieux.
+        ${cv.propos}
     </p>
     </div>
     <div class="section">
     <h2>COMPÉTENCES</h2>
     <ul class="skills">
-        <li><img src="icon/check.png" alt="check"> Suite Office</li>
-        <li><img src="icon/check.png" alt="check"> Informatique général</li>
-        <li><img src="icon/check.png" alt="check"> Relationnel</li>
-        <li><img src="icon/check.png" alt="check"> Polyvalent</li>
+        ${skills}
     </ul>
     </div>
     <div class="section">
     <h2>Langues</h2>
     <p>
-        Français, langue Maternelle
-        <br>
-        Anglais, niveaux A2
+        ${langues}
     </p>
     </div>
     <div class="section">
     <h2>Centre d'intérêt</h2>
-    <p>
-        Jeux vidéo, jouer et comprendre
-        <br>
-        Fauconnerie
-        <br>
-        Informatique en général
-        <br>
-        Culture Japonaise
-        <br>
-        Aviation, aéronautique
-    </p>
+    <ul>
+        ${centre}
+    </ul>
     </div>
 </div>
 <div class="colonne-droite">
     <div class="header">
-    <h1>Andy <span class="text-vert text-uppercase">Petitjean</span></h1>
-    <p>Web Developer</p>
+    <h1>${cv.prenom} <span class="text-vert text-uppercase">${cv.nom}</span></h1>
+    <p>${cv.fonction}</p>
     <ul class="infos">
-        <li><img src="icon/e-mail.png" alt="Mail"><a href="mailto:andywinga@hotmail.be"> andywinga@hotmail.be</a></li>
-        <li><img src="icon/telephone-portable.png" alt="telephone-portable"> 0475 69 07 07</li>
-        <li><img src="icon/carte.png" alt="Carte"> Rue du je me suis perdu 5, 1313 Aidez-moi</li>
+        <li><img src="icon/e-mail.png" alt="Mail"><a href="mailto:${cv.mail}"> ${cv.mail}</a></li>
+        <li><img src="icon/telephone-portable.png" alt="telephone-portable"> ${cv.telephone}</li>
+        <li><img src="icon/carte.png" alt="Carte"> ${cv.adresse}</li>
     </ul>
     </div>
     <div class="content">
-    <div class="section">
-        <h2>Expériences <span class="text-vert">professionnelles</span></h2>
-        <p>
-        <strong>2019 <img src="icon/prochain.png" alt="fleche-droite"> 2020</strong>
-        <br>
-        Responsable d'équipe chez <em class="text-vert">Domino's Wavre</em>
-        </p>
-        <ul class="liste-d-experience">
-        <li>Management d'équipe</li>
-        <li>Responsabilité de la caisse</li>
-        <li>Gestion des stocks</li>
-        <li>Ouverture/Fermeture du magasin</li>
-        <li>Organisation des livraison/production</li>
-        <li>Optimisation des tâches quotidiennes</li>
-        </ul>
-    </div>
-    <div class="section">
-        <p>
-        <strong>2017</strong>
-        <br>
-        Employé polyvalent chez <em class="text-vert">MSGM Lukoil Bierges</em>
-        </p>
-        <ul class="liste-d-experience">
-        <li>Gestion de caisse</li>
-        <li>Ouverture/fermeture du magasin</li>
-        <li>Réassort des rayons</li>
-        <li>Gestion de la loterie</li>
-        <li>Gestion des pompes</li>
-        <li>Gestion des factures clients</li>
-        </ul>
-    </div>
-    <div class="section">
-        <p>
-        <strong>2016</strong>
-        <br>
-        Livreur chez <em class="text-vert">BPOST</em>
-        </p>
-        <ul class="section">
-        <li>Gestion des tournées exceptionnelles</li>
-        <li>Livraise des clients</li>
-        </ul>
-    </div>
-    <div class="section">
-        <p>
-        <strong>2011 <img src="icon/prochain.png" alt="fleche-droite"> 2016</strong>
-        <br>
-        Différentes entreprises
-        </p>
-        <ul class="liste-d-experience">
-        <li>Délégué Commercial <em class="text-vert">Proximédia</em></li>
-        <li>Employé polyvalent <em class="text-vert">McDonald's Waterloo</em></li>
-        <li>Agent d'accueil <em class="text-vert">CSC</em></li>
-        <li>Ect...</li>
-        </ul>
-    </div>
+    <h2>Expériences <span class="text-vert">professionnelles</span></h2>
+    ${experiences}
     <div class="section">
         <h1>Études <span class="text-vert">&Formation</span></h1>
-        
-        <p><strong>2023 <img src="icon/prochain.png" alt="fleche-droite"> 2025</strong> <em class="text-vert">Ifosup Wavre</em> <br> <em>BES Web Developer</em> <br> Anglais UE-2</p>
-        <p><strong>2012 <img src="icon/prochain.png" alt="fleche-droite"> 2015</strong> <em class="text-vert">CEFA Court-Saint-Etienne</em> <br> CESS <br> Qualification d'auxiliaire administratif d'aide et d'accueil <br> Gestion d'entreprises </p>
+        ${formation}
     </div>
-    <div class="section">
+    <div class="section" id="autres">
         <h2>Autres <span class="text-vert">Expériences</span></h2>
-        <p>
-        
-        Permis B
-        <br>
-        Animateur de Centre de Vacances durant 2 ans
-        <br>
-        Brevet d'animateur de Centre de Vacances
-        </p>
+        <ul>
+        ${autres}
+        </ul>
     </div>
     </div>
 </div>
